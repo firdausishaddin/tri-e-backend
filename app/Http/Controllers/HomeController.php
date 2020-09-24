3d1469f05ec1f22df6a53d198ca85a8efbc1de6d
Firdaus Ishaddin <firdausishaddin@gmail.com>
2020-09-24 17:28:35 +0800

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Uniques;
 use Illuminate\Http\Request;
 use DB;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
@@ -35,27 +36,37 @@ class HomeController extends Controller
 
         DB::beginTransaction();
         // try {
-            foreach (array_chunk($input, 1000) as $data) {
-                $existingDatas = DB::table('uniques')->whereIn('unique_code', $data)->get();
 
-                if(count($existingDatas) == 0) {
-                    foreach($data as $key => $value) {
-                        // dd($value);
-                        if (!DB::table('uniques')->insert($data)) throw new Exception("Error Processing Request", 1);
-                    }
-                } else {
-                    $str = Str::random(6);
+        foreach (array_chunk($input, 50000) as $data) {
 
-                    DB::table('uniques')->insert(['unique_code' => $str]);
-                }
-            }
+            // $existingDatas = DB::table('uniques')->whereIn('unique_code', $data)->get();
 
-            DB::commit();
-            return response()->json([
-                'success' => true,
-                'message' => 'Success',
-                'data' => $input,
-            ], 200);
+            // if(count($existingDatas) != 0) {
+            //     $filteredArrays = Arr::get($data, 'unique_code', $existingDatas);
+
+            //     if(!$filteredArrays) {
+            //         if (!DB::table('uniques')->insert($data)) throw new Exception("Error Processing Request", 1);
+            //     } else {
+            //         foreach($filteredArrays as $filteredArray) {
+            //             $str = Str::random(6);
+
+            //             $test = array_replace([$filteredArray], ['unique_code' => $str]);
+
+            //             DB::table('uniques')->insert(['unique_code' => $test['unique_code']]);
+            //         }
+            //     }
+            //     DB::table('uniques')->insert($data);
+            // }
+
+            $this->hasDupes($data);
+        }
+
+        DB::commit();
+        return response()->json([
+            'success' => true,
+            'message' => 'Success',
+            'data' => $input,
+        ], 200);
         // } catch (\Exception $th) {
         //     $thFullMessage = $th->getMessage();
         //     $thMessage = substr($thFullMessage, 0, strpos($thFullMessage, ' ('));
@@ -69,21 +80,34 @@ class HomeController extends Controller
         // }
     }
 
-    public function hasDupes($arr)
+    public function hasDupes($data)
     {
-        $arrCnt = $request->digit-count($arr);
-        
-        if($arrCnt != 0) {
-            for($i = 1; $i <= $arrCnt; $i++) {
-                $str = Str::random(6);
+        $existingDatas = DB::table('uniques')->whereIn('unique_code', $data)->get();
+        if (count($existingDatas->toArray()) > 0) {
+            $filteredArrays = Arr::get($data, 'unique_code', $existingDatas);
 
-                $arr[] = ['unique_code' => $str];                
+            foreach ($filteredArrays as $key => $filteredArray) {
+                $str = Str::random(6);
+                $test = array_replace([$filteredArrays[$key]], ['unique_code' => $str]);
+                unset($data[$key]);
+                DB::table('uniques')->insert(['unique_code' => $test['unique_code']]);
+            }
+
+            foreach ($data as $val => $c) {
+                if ($c > 1) {
+                    $str = Str::random(6);
+                    unset($c);
+                    DB::table('uniques')->insertOrIgnore(['unique_code' => $str]);
+                }
+            }
+        } else {
+            foreach ($data as $val => $c) {
+                if ($c > 1) {
+                    $str = Str::random(6);
+                    unset($c);
+                    DB::table('uniques')->insertOrIgnore(['unique_code' => $str]);
+                }
             }
         }
-    }
-
-    public function regenerate($arr)
-    {
-        // 
     }
 }
